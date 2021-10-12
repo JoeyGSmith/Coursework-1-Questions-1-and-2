2bc9d750ecbab9a29ce929bdd1a07649f2e4755b
JoeyGSmith <92368479+JoeyGSmith@users.noreply.github.com>
2021-10-12 11:36:14 +0100

    Update linear_algebra.cpp
    This file contains the function that performs the Biconjugate Gradient Stabilised method, which is used to find the vector x in the equation xA = b. This code works for A as a tridiagonal matrix, and takes as inputs the 3 vectors representing A, and the target vector b, and outputs the vector x.

--- linear_algebra.cpp
+++ linear_algebra.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include "general.hpp"
 
+
 double* A_vec_multiplication(double* A0, double* A1, double* A2, int n, double* vec);
 double Dot_Product(double* x, double* y, int n);
 double vec_Norm(double*x, int n);
